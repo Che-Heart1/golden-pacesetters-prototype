038ac21d49d244134784e6b21ba67175ee8c3e85
Che-Heart1 <iyin.fashola@gmail.com>
2026-06-08 00:37:23 +0000

    feat: complete TopNavBar

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+  var nav = document.getElementById('topnav');
+
+  function onScroll() {
+    nav.classList.toggle('scrolled', window.scrollY > 20);
+  }
+
+  window.addEventListener('scroll', onScroll, { passive: true });
+  onScroll();
+}());
